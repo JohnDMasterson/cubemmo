cf7dcddb46da49aae009d435b98afe13d6163a3d
JohnDMasterson <john@localhost.localdomain>
2014-04-27 22:40:02 -0500

    basic webgl implemented

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,49 +1,174 @@
 // Startup websocket connection
-var socket = io.connect('/'),
-	username = "";
+var socket = io.connect('/');
+
+// Initializes position for the user;
+var postion = {};
+
+//gl instance
+var gl;
+
+var triangleVertexPositionBuffer;
+
+var squareVertexPositionBuffer;
+
+var mvMatrix = mat4.create();
+
+var pMatrix = mat4.create();
+
+
+//used for getting files from server
+var getSourceSync = function(url) {
+		var req = new XMLHttpRequest();
+		req.open("GET", url, false);
+		req.send(null);
+		return (req.status == 200) ? req.responseText : null;
+};
+
 
 $(document).ready(function (){
+	window.addEventListener("load", webGLStart(), true);
 
-	// Get username from client
-	username = prompt("Please enter your name");
-
-	// Define callback for when a message is sent over websockets
-	socket.on('incoming_message', function (message) {
-
-		// Append the output window with the message.
-
-		// Javascript injection prevention
-		var username = document.createElement('strong');
-		username.appendChild(document.createTextNode(message.username));
-		var content = document.createTextNode(': ' + message.content);
-		var br = document.createElement('br');
-		// Append to console
-    	$('.output').append(username);
-    	$('.output').append(content);
-    	$('.output').append(br);
-
-    	// Scroll the div to the most recently appended message
-		$('.output').scrollTop($('.output')[0].scrollHeight);
-	});
-
-	// Let everyone know you joined the chat server. It is client side and easily removable but it is not a security risk.
-	sendMessage("Hi! I just joined");
-
-	// Get input text when the user hits the enter key
-	$('.input').on('keypress', function (event) {
-		if(event.keyCode == 13){
-			event.preventDefault();
-
-			// Send message containing the value of the input box
-			sendMessage($(this).val());
-
-			// Clear the value of the input box
-			$(this).val('');
-		}
-	});
 });
 
-// Send message to the server with my username and message. Username forgery is easy but lets not worry about that for now. We can handle with sessions later
-function sendMessage(content){
-	socket.emit('relay_me', {username:username, content:content});
+
+
+function initGL(canvas) {
+	try {
+		gl = canvas.getContext("webgl");
+		gl.viewportWidth = canvas.width;
+		gl.viewportHeight = canvas.height;
+	}
+	catch (e) {
+	}
+	
+	if(!gl) {
+		alert ("Could not initialize WebGL");
+	}
 }
+
+function getShader(gl, glslcode, type) { 
+
+
+    var shader;
+    if (type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+    	alert("failure to create shader");
+        return null;
+    }
+
+    gl.shaderSource(shader, glslcode);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader));
+
+        return null;
+    }
+
+
+    return shader;
+}
+
+
+function initShaders() {
+	var fsource = getSourceSync("shaders/defualt.frag");
+	var vsource = getSourceSync("shaders/default.vert");
+
+    var fragmentShader = getShader(gl, fsource, "x-shader/x-fragment");
+    var vertexShader = getShader(gl, vsource, "x-shader/x-vertex");
+
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Could not initialise shaders");
+    }
+
+    gl.useProgram(shaderProgram);
+
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+    shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+    shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+}
+
+
+
+function setMatrixUniforms() {
+    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+function initBuffers() {
+    triangleVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+    var vertices = [
+         0.0,  1.0,  0.0,
+        -1.0, -1.0,  0.0,
+         1.0, -1.0,  0.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    triangleVertexPositionBuffer.itemSize = 3;
+    triangleVertexPositionBuffer.numItems = 3;
+
+    squareVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    vertices = [
+         1.0,  1.0,  0.0,
+        -1.0,  1.0,  0.0,
+         1.0, -1.0,  0.0,
+        -1.0, -1.0,  0.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    squareVertexPositionBuffer.itemSize = 3;
+    squareVertexPositionBuffer.numItems = 4;
+}
+
+
+function drawScene() {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+    mat4.identity(mvMatrix);
+
+    mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+
+
+    mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+}
+
+
+
+function webGLStart() {
+	var canvas = document.getElementById("cubemmo-canvas");
+    initGL(canvas);
+    initShaders();
+    initBuffers();
+
+	
+	//background color, black
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	//enables 3D stuff
+	gl.enable(gl.DEPTH_TEST);
+	
+	drawScene();
+}
+
+
+	
